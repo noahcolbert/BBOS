e4e9e843af49b1691c73d11fa9e73593776d81a8
noahcolbert <noah@noahcolbert.com>
2025-10-21 13:06:10 -0400

    completed skeleton code, working on making it compile

---
+++ iso.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/bbos.kernel isodir/boot/bbos.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "bbos" {
+    multiboot /boot/bbos.kernel
+}
+EOF
+grub-mkrescue -o bbos.iso isodir
+
