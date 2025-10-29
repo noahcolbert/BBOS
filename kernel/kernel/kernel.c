269657b72a8b956f9966ce427a37cb6d1ddb0f62
noahcolbert <noah@noahcolbert.com>
2025-10-29 14:06:17 -0400

    Began Page Frame Allocator logic

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stddef.h>
 
 #include <kernel/tty.h>
+
+extern __uint32_t endkernel;
+
+// TODO: Add logic for page framing
+
 
 void kernel_main(void) {
 	terminal_initialize();
