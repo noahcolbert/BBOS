1d6ab3873345ac8e9d97fe0ff26260883cafdb75
noahcolbert <noah@noahcolbert.com>
2025-10-31 14:32:23 -0400

    happy halloween starting memmap implementation

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,9 +6,17 @@
 extern __uint32_t endkernel;
 
 // TODO: Add logic for page framing
+	//I gotta fo a lot of memmap stuff first my b
 
 
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello Kernel world!\n");
+
+	/* Ordered list of things I gotta do */
+
+	// get system info and memmap
+
+	// initialise a page frame allocator
+
 }
---
+++ libc/include/sys/memmap.h
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stddef.h>
+
+/*  Incredibly primitive while I'm learning
+    Lets see how much this changes lol */
+
+
+memmap_entry_t create_mmap_entry(uint32_t start, uint32_t size, uint32_t flags);
+
+memory_map_t* generate_memmap(void* multiboot_info, sysinfo_t* sysinfo);
+
+uint32_t sort_memmap(memory_map_t* mmap);
+
+void condense_memmap(memory_map_t* mmap);
