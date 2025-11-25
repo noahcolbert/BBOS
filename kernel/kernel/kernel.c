f7b0fdc7e8804a3863c5d17651f541cb9acc1e6e
noahcolbert <noah@noahcolbert.com>
2025-11-25 16:55:03 -0500

    refactored  code, getting a seg fault at _Start

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -7,51 +7,16 @@
 
 #include <kernel/tty.h>
 
-extern void loadPageDirectory(long unsigned int*);
-extern void enablePaging();
+extern void initialize_paging();
+extern void enable_paging();
 
 extern uint32_t endkernel;
-uint32_t page_directory[1024] __attribute__((aligned(4096)));
-uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
 
 // TODO: Add logic for page framing
 	//I gotta fo a lot of memmap stuff first my b | I DID THIS
 	//I gotta move to da higher half now
 
-
-void populate_pagetable(void) {
-	// holds the physical address where we want to start mapping these pages to.
-	// in this case, we want to map these pages to the very beginning of memory.
-	unsigned int i;
-
-	//we will fill all 1024 entries in the table, mapping 4 megabytes
-	for(i = 0; i < 1024; i++)
-	{
-		// As the address is page aligned, it will always leave 12 bits zeroed.
-		// Those bits are used by the attributes ;)
-		first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
-	}
-}
-
-void init_pageframe(void) {
-	//set each entry to not present
-	int i;
-	for(i = 0; i < 1024; i++)
-	{
-		// This sets the following flags to the pages:
-		//   Supervisor: Only kernel-mode can access them
-		//   Write Enabled: It can be both read from and written to
-		//   Not Present: The page table is not present
-		page_directory[i] = 0x00000002;
-	}
-	printf("Blanked Page dir\n");
-	populate_pagetable();
-	// attributes: supervisor level, read/write, present
-	page_directory[0] = ((unsigned int)first_page_table) | 3;
-	loadPageDirectory(page_directory);
-	enablePaging();
-	printf("Paging enabled\n");
-}
 
 void kernel_main(void) {
 
@@ -86,7 +51,8 @@ void kernel_start(multiboot_info_t* mbd, unsigned int magic) {
 	}
 
 	show_memmap(mbd);
-	//init_pageframe();
+	initialize_paging();
+	enable_paging();
 	kernel_main();
 }
 
