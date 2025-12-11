8f3bab02329a0afe6312ced453a4a0c19e0cab3a
noahcolbert <noah@noahcolbert.com>
2025-12-11 12:47:20 -0500

    added remote gdb support to qemu startup script

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -m 4G -cdrom bbos.iso -monitor stdio
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -s -S -m 4G -cdrom bbos.iso -monitor stdio
